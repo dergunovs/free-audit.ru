50f0b51f54c32186b3409eda41c501c8659b4fcd
Aleksandr Dergunov <dergunovs@mail.ru>
2020-10-03 15:36:08 +0300

    удаление результатов аудита на странице списка

--- pages/result/index.vue
+++ pages/result/index.vue
@@ -4,10 +4,13 @@
     <div class="list-block">
       <div v-for="result in results" :key="result.index" class="list-item">
         <nuxt-link :to="`/result/${result._id}`" class="list-item-link">{{ result.url }}</nuxt-link>
-        {{ result.audit._id.name }}, {{ $dateFns.format(new Date(result.date_created), "dd.MM.yyyy г. HH:mm") }}
-        <span v-if="result.passwordCreated">
-          - <b>Доступ создан</b>, <a :href="`mailto:${result.email}`">{{ result.email }}</a>
-        </span>
+        <span v-on:click="resultDelete(result._id)" class="list-item-delete">X</span>
+        <div>
+          {{ result.audit._id.name }}, {{ $dateFns.format(new Date(result.date_created), "dd.MM.yyyy г. HH:mm") }}
+          <span v-if="result.passwordCreated">
+            <a :href="`mailto:${result.email}`">{{ result.email }}</a>
+          </span>
+        </div>
       </div>
     </div>
   </div>
@@ -34,6 +37,29 @@ export default {
   },
   head: {
     title: "Список результатов аудитов"
+  },
+  methods: {
+    resultDelete(resutlId) {
+      axios
+        .delete(`${process.env.baseUrl}/api/result/${resutlId}`, {
+          headers: {
+            Authorization: this.$auth.$storage._state["_token.local"]
+          }
+        })
+        .then(response => {
+          axios
+            .get(`${process.env.baseUrl}/api/result/`, {
+              headers: {
+                Authorization: this.$auth.$storage._state["_token.local"]
+              }
+            })
+            .then(response => {
+              this.results = response.data;
+            });
+          this.$toast.success("Готово", { duration: 1000 });
+        })
+        .catch(err => this.$toast.error(err.response.data.message, { duration: 5000 }));
+    }
   }
 };
 </script>
